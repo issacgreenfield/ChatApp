ab4f256019d58d6b3a199d5f54c912ce87191e4b
Issac Greenfield <issacgreenfield@gmail.com>
2018-06-03 15:36:22 -0700

    Application bones are running on the server

---
+++ WebContent/index.jsp
@@ -0,0 +1,28 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<link type="text/css" rel="stylesheet" href="style.css" />
+
+<title>Greenfield Chat Application</title>
+</head>
+<body>
+<div id="wrapper">
+    <div id="menu">
+        <p class="welcome">Welcome to Greenfield Chat Application<b></b></p>
+        <p class="login"><a id="exit" href="#">Login to Chat</a></p>
+        <div style="clear:both"></div>
+    </div>
+     
+    <div id="chatbox"></div>
+     
+    <form name="message" action="">
+        <input name="usermsg" type="text" id="usermsg" size="63" />
+        <input name="submitmsg" type="submit"  id="submitmsg" value="Send" />
+    </form>
+</div>
+
+</body>
+</html>
---
+++ src/com/greenfield/servlet/ChatServlet.java
@@ -0,0 +1,49 @@
+package com.greenfield.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ChatServlet
+ */
+@WebServlet("/chat.html")
+public class ChatServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+//    /**
+//     * @see HttpServlet#HttpServlet()
+//     */
+//    public ChatServlet() {
+//        super();
+//        // TODO Auto-generated constructor stub
+//    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		PrintWriter toOut = response.getWriter();
+		toOut.println("<html>");
+		toOut.println("<body>");
+		toOut.println("<Servlet is running>");
+		toOut.println("</body>");
+		toOut.println("</html>");
+	}
+
+//	/**
+//	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+//	 */
+//	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		// TODO Auto-generated method stub
+//		doGet(request, response);
+//	}
+
+}
